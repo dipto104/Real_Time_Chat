07638ab7515565d472908b9b3d917463f5780e5e
dipto104 <diptoroy.roy1912@gmail.com>
2019-07-10 15:02:25 +0600

    1st commit

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+var io = require('socket.io').listen(server);
+
+users=[];
+connections=[];
+
+server.listen(process.env.PORT || 3000);
+console.log("server running ...")
+
+app.get('/',function(req,res) {
+	res.sendFile(__dirname + '/index.html');
+	// body...
+});
+
+io.sockets.on('connection',function(socket){
+	connections.push(socket);
+	console.log('connected : %s sockets connected', connections.length);
+
+	//disconnected
+	socket.on('disconnect',function(data){
+		//if(!socket.username) return;
+
+		users.splice(users.indexOf(socket.username),1);
+		Updateusername()
+
+		connections.splice(connections.indexOf(socket),1);
+		console.log('Disconnected : %s sockets connected',connections.length);
+	});
+	//send message
+	socket.on('send message',function(data){
+		//console.log(data);
+		io.sockets.emit('new message',{msg:data});
+	});
+
+	//new user
+	socket.on('new user',function(data,callback){
+		callback(true);
+		socket.username=data;
+		users.push(socket.username);
+		Updateusername();
+	});
+
+	function Updateusername(){
+		io.sockets.emit('get users',users)
+	}
+})
